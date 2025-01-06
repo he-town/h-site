502a703fea975d9ef6b4c4f05dece36dd0f4862a
he-town <hannaheichinger@outlook.com>
2025-01-06 18:00:45 -0500

    adding all changes to source ctrl to continue working on desktop

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,15 @@ import { FileRoutes } from "@solidjs/start/router";
 import { Suspense } from "solid-js";
 import Nav from "~/components/Nav";
 import "./app.css";
+import Newsfeed from "./components/Newsfeed/Newsfeed";
 
 export default function App() {
   return (
     <Router
-      root={props => (
+      root={(props) => (
         <>
           <Nav />
+          <Newsfeed />
           <Suspense>{props.children}</Suspense>
         </>
       )}
---
+++ src/components/Newsfeed/Newsfeed.tsx
@@ -0,0 +1,10 @@
+const Newsfeed = () => {
+  return <div class="container p-4">Welcome to my website!</div>;
+};
+
+export default Newsfeed;
+
+// fill entire width of container
+// come down to fill 10% of container height
+
+// need to learn more about em units
