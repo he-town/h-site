e3fe16cabf2c4e7393d4100072352412591e75e3
he-town <hannaheichinger@outlook.com>
2025-01-07 14:24:36 -0500

    added a custom font to proj

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,5 @@
+const Header = () => {
+  return <div class="text-8xl p-12 font-oi">h-town</div>;
+};
+
+export default Header;
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
   theme: {
-    extend: {}
+    extend: {
+      fontFamily: {
+        oi: ["Oi"],
+        // Add more custom font families as needed
+      },
+    },
   },
-  plugins: []
+  plugins: [],
 };
