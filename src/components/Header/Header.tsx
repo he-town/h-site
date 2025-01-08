9917ca50eaa352ccce8cfa4d5bc394de605ee625
he-town <hannaheichinger@outlook.com>
2025-01-07 19:00:12 -0500

    applied color gradient to heading, added nice color palette to app.css

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,7 @@
+import "./Header.css";
+
 const Header = () => {
-  return <div class="text-8xl p-12 font-oi">h-town</div>;
+  return <div class="text-8xl p-12 font-oi header-color">h-town</div>;
 };
 
 export default Header;
