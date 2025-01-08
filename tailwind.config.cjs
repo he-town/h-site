c985c1d307374f20f1aa342b9592bc23cf1f117c
he-town <hannaheichinger@outlook.com>
2025-01-07 19:20:33 -0500

    added colors to tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,13 @@
 module.exports = {
   content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
   theme: {
+    colors: {
+      my_red: "#ff595e",
+      my_yellow: "ffca3a",
+      my_green: "#8ac926",
+      my_blue: "#1982c4",
+      my_purple: "#6a4c93",
+    },
     extend: {
       fontFamily: {
         oi: ["Oi"],
