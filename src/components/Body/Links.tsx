ac5faa0d4993b6e44fde9074ab7f6e3826aaf707
he-town <hannaheichinger@outlook.com>
2025-01-09 12:38:07 -0500

    responsive font size on header!

--- src/components/Body/Links.tsx
+++ src/components/Body/Links.tsx
@@ -15,9 +15,3 @@ export default Links;
 
 // TODO abstract code reuse
 // TODO should linkText be a prop or child? ...
-
-/*
-Saturday Jan 18, 10am - 12:30pm  (Ryder 354)
-Saturday Feb 8, 9;30am – 11:30am (Ryder 354)
-Tuesday Mar. 18, 6pm - 8:15pm  (Fenway Center)
-*/
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,7 @@
 import Newsfeed from "../Newsfeed/Newsfeed";
 import "./Header.css";
 
-const Header = () => (
-  <div class="text-8xl pl-4 pt-12 font-oi header-color">h-town</div>
-);
+const Header = () => <div class="pl-4 pt-12 font-oi header-color">h-town</div>;
 
 export default Header;
 
