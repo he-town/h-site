df223bf1a6eaa3d375f842e2224b44e333ad25d4
he-town <hannaheichinger@outlook.com>
2025-01-09 15:03:17 -0500

    deleted some old imports, began refactoring about page

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,6 @@
 import { Router } from "@solidjs/router";
 import { FileRoutes } from "@solidjs/start/router";
 import { Suspense } from "solid-js";
-import Nav from "~/components/Nav";
 import "./app.css";
 import Newsfeed from "./components/Newsfeed/Newsfeed";
 import Header from "./components/Header/Header";
