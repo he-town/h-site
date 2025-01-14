b5e994c8124d127cdf5b68e69e3acc56b2152a39
he-town <hannaheichinger@outlook.com>
2025-01-14 14:18:44 -0500

    nicer pfp section

--- src/components/Body/PFP.tsx
+++ src/components/Body/PFP.tsx
@@ -1,10 +1,12 @@
-import me from "/home/htown/Code/me.png";
+import me from "/home/htown/Code/me-crop.png";
 
 const PFP = () => {
   return (
     <div class="w-2/5 border-2 h-full">
-      <img src={me} />
-      <div>Welcome to my page! </div>
+      <img src={me} class="p-12 rounded-full" />
+      <div class="text-center" style="font-size: clamp(0.05rem, 2.2vw, 2rem);">
+        Welcome to my page!
+      </div>
     </div>
   );
 };
