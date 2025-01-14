ec2c3cf1fce33ed3d8e00229846703efabde3e09
he-town <hannaheichinger@outlook.com>
2025-01-14 13:54:58 -0500

    learning how image imports work

--- src/components/Body/PFP.tsx
+++ src/components/Body/PFP.tsx
@@ -1,10 +1,14 @@
+import me from "/home/htown/Code/me.png";
+
 const PFP = () => {
   return (
     <div class="w-2/5 border-2 h-full">
-      <img src="" />
+      <img src={me} />
       <div>Welcome to my page! </div>
     </div>
   );
 };
 
 export default PFP;
+
+// "~/..." doesn't work
