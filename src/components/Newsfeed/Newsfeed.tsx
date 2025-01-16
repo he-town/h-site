2d4b0e26b2d06713cc0485dcf460750471822f16
he-town <hannaheichinger@outlook.com>
2025-01-16 16:33:17 -0500

    see how it works better now, but text is still jittery

--- src/components/Newsfeed/Newsfeed.tsx
+++ src/components/Newsfeed/Newsfeed.tsx
@@ -1,16 +1,25 @@
-import { gsap } from "gsap";
+import "./News.css";
 import horizontalLoop from "./GSAPLoop";
 import { onMount } from "solid-js";
 
 const Newsfeed = () => {
   let contentRef!: HTMLDivElement;
   onMount(() => {
-    const tl = horizontalLoop(contentRef, { repeat: -1 });
+    const tl = horizontalLoop(".news-piece", { repeat: -1 });
   });
 
   return (
-    <div class="mx-4 p-4 border-2">
-      <div ref={contentRef}>My content</div>
+    <div class="border-2 flex overflow-hidden mx-4 p-4 items-center justify-between">
+      <div class="news-piece">Coffee</div>
+      <div class="news-piece">Latte</div>
+      <div class="news-piece">Espresso</div>
+      <div class="news-piece">Mocha</div>
+      <div class="news-piece">Cortado</div>
+      <div class="news-piece">Macchiato</div>
+      <div class="news-piece">Flat White</div>
+      <div class="news-piece">Cold Brew</div>
+      <div class="news-piece">Nitro</div>
+      <div class="news-piece">Dunks</div>
     </div>
   );
 };
