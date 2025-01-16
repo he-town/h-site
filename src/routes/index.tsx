855161cb767a016bb8fc5add91764ce703ee5fdc
he-town <hannaheichinger@outlook.com>
2025-01-16 17:16:22 -0500

    newsfeed added to main page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,11 @@
 import Body from "~/components/Body/Body";
+import Newsfeed from "~/components/Newsfeed/Newsfeed";
 
 export default function Home() {
-  return <Body />;
+  return (
+    <>
+      <Newsfeed />
+      <Body />
+    </>
+  );
 }
