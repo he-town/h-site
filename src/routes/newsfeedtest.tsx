9faf7413d85d953bc5007525da0cb868cfc2de0a
he-town <hannaheichinger@outlook.com>
2025-01-16 11:17:41 -0500

    experiment showing some style things. also adding gsap to project

--- src/routes/newsfeedtest.tsx
+++ src/routes/newsfeedtest.tsx
@@ -1,5 +1,12 @@
+import { gsap } from "gsap";
+
 const Page = () => {
-  return <div>WIP</div>;
+  return (
+    <div class="flex flex-row border-2">
+      <div class="mx-4 border-2">WIP</div>
+      <div class="mx-4 border-2">WIP</div>
+    </div>
+  );
 };
 
 export default Page;
