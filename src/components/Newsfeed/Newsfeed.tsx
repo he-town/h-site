b910d8d625630150d91ec85dcda53d8914b45fbe
he-town <hannaheichinger@outlook.com>
2025-01-16 13:05:08 -0500

    marquee is moving but not as intended; note to self: must use div

--- src/components/Newsfeed/Newsfeed.tsx
+++ src/components/Newsfeed/Newsfeed.tsx
@@ -1,12 +1,18 @@
 import { gsap } from "gsap";
 import horizontalLoop from "./GSAPLoop";
+import { onMount } from "solid-js";
 
 const Newsfeed = () => {
-  const myContent = <span>My content</span>;
-  // const scrollingText = gsap.utils.toArray("span");
-  // const tl = horizontalLoop(scrollingText, { repeat: -1 });
+  let contentRef!: HTMLDivElement;
+  onMount(() => {
+    const tl = horizontalLoop(contentRef, { repeat: -1 });
+  });
 
-  return <div class="mx-4 p-4 border-2">{myContent}</div>;
+  return (
+    <div class="mx-4 p-4 border-2">
+      <div ref={contentRef}>My content</div>
+    </div>
+  );
 };
 
 export default Newsfeed;
