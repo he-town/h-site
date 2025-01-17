629fe599d1b42e5917a654b9748096d60379c87c
he-town <hannaheichinger@outlook.com>
2025-01-17 11:36:12 -0500

    styling changes

--- src/components/Body/PFP.tsx
+++ src/components/Body/PFP.tsx
@@ -1,9 +1,9 @@
 const PFP = () => {
   return (
-    <div class="w-2/5 border-2 h-full rounded-lg font-average_sans">
+    <div class="w-2/5 border-2 h-full rounded-lg font-average_sans transition-all duration-500 ease-in-out hover:bg-gray-300">
       <div
         class="text-center mb-8"
-        style="font-size: clamp(0.05rem, 2.2vw, 2rem);"
+        style="font-size: clamp(0.05rem, 2.7vw, 2rem);"
       >
         I'm Hannah Eichinger!
       </div>
