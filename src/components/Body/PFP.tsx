b8b539c240a41f94a49058f2079a3088f4e33b86
he-town <hannaheichinger@outlook.com>
2025-01-22 13:14:28 -0500

    pfp brought in from discord cdn link (temp)

--- src/components/Body/PFP.tsx
+++ src/components/Body/PFP.tsx
@@ -1,9 +1,12 @@
-import me from "/home/htown/Code/me-crop.png";
-
 const PFP = () => {
   return (
     <div class=" border-2 h-full rounded-lg font-average_sans w-2/5">
-      <img src={me} class="p-12 rounded-full" />
+      <img
+        src={
+          "https://media.discordapp.net/attachments/993253169268260876/1331687552872222851/me-crop.png?ex=6792862e&is=679134ae&hm=a9676ebe4d98b510f27e00299389925d3da5195a76cdf56913067a3d3eed2f45&=&format=webp&quality=lossless&width=862&height=862"
+        }
+        class="p-12 rounded-full"
+      />
       <div
         class="text-center mb-8"
         style="font-size: clamp(0.05rem, 2.7vw, 2rem);"
